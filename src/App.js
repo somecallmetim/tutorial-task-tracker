e408732fe83d7898f56836a737137e20f5266502
Tim Bauer <timbauer@ymail.com>
2021-10-18 12:18:11 -0700

    completed through PropTypes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
+import Header from "./components/Header";
+
 function App() {
-  const name = 'Brad'
-  const bool = false
+  const name = "Brad";
+  const bool = false;
   return (
     <div className="container">
-      <h1>Random and quirky String</h1>
-      <h2>Hello {name}</h2>
-      <h2>I am { bool ? 'Yes' : 'No' }</h2>
+      <Header title="I'm a title!!!"/>
     </div>
   );
 }
